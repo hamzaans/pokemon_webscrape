111d3c52b8447050219daf47fb19ddac1f5539cf
hamzaans <66072309+hamzaans@users.noreply.github.com>
2020-07-15 16:54:44 -0400

    Add files via upload

--- pokemon_home_webscrape.py
+++ pokemon_home_webscrape.py
@@ -12,25 +12,42 @@ from selenium.webdriver.firefox.options import Options
 from datetime import datetime
 from datetime import date
 
-driver = webdriver.Firefox()
-pokemon_list = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Squirtle', 'Wartortle', 'Blastoise', 'Mewtwo', 'Mew', 'Celebi', 'Jirachi', 'Cobalion', 'Terrakion', 'Virizion', 'Reshiram', 'Zekrom', 'Kyurem', 'Keldeo', 'Rowlet', 'Dartrix', 'Decidueye', 'Litten', 'Torracat', 'Incineroar', 'Popplio', 'Brionne', 'Primarina', 'Cosmog', 'Cosmoem', 'Solgaleo', 'Lunala', 'Necrozma', 'Marshadow', 'Zeraora', 'Meltan', 'Melmetal']
+options = Options()
+options.headless = False
+pokemon_home_jsons = []
+i = 0
+while i < 36:
+    driver = webdriver.Firefox(options=options)
+    pokemon_list = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Squirtle', 'Wartortle', 'Blastoise', 'Mewtwo', 'Mew', 'Celebi', 'Jirachi', 'Cobalion', 'Terrakion', 'Virizion', 'Reshiram', 'Zekrom', 'Kyurem', 'Keldeo', 'Rowlet', 'Dartrix', 'Decidueye', 'Litten', 'Torracat', 'Incineroar', 'Popplio', 'Brionne', 'Primarina', 'Cosmog', 'Cosmoem', 'Solgaleo', 'Lunala', 'Necrozma', 'Marshadow', 'Zeraora', 'Meltan', 'Melmetal']
 
-driver.get('https://serebii.net')
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("Bulbasaur")
-elem.send_keys(Keys.RETURN)
-time.sleep(3)
+    driver.get('https://serebii.net')
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys(pokemon_list[i])
+    elem.send_keys(Keys.RETURN)
+    time.sleep(3)
 
-driver.find_element_by_xpath('//*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a').click()
+    driver.find_element_by_xpath('//*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div/a').click()
+    time.sleep(2)
+    driver.close()
 
-time.sleep(3)
+    window_after = driver.window_handles[0]
+    driver.switch_to.window(window_after)
+    time.sleep(5)
 
-window_after = driver.window_handles[1]
-driver.switch_to.window(window_after)
+    #Want names, image, and abilities
+    name = driver.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/table[5]/tbody/tr[2]/td[1]').get_attribute('innerHTML')
+    image = 'https://www.serebii.net/' +  (driver.find_element_by_xpath('//*[@id="content"]/main/div/div/table[4]/tbody/tr[2]/td/table/tbody/tr/td[1]/img').get_attribute('outerHTML').split('src="')[1].split('">')[0])
+    abilities = driver.find_element_by_xpath('//*[@id="content"]/main/div/div/table[6]/tbody/tr[2]/td').find_elements_by_tag_name('a')
+    
+    abilities_list = []
+    for ability in abilities:
+        abilities_list.append((ability.get_attribute('innerHTML')).split('<b>')[1].split('</b>')[0])
+        # print((ability.get_attribute('innerHTML')).split('<b>')[1].split('</b>')[0])
+    pokemon_home_json = {"name": name, "image": image, "abilities": abilities_list}
+    # print(pokemon_json)
+    pokemon_home_jsons.append(pokemon_home_json)
+    i = i + 1
+    driver.close()
 
-#Want names, image, and abilities
-name = driver.find_element_by_xpath('/html/body/div[1]/div[2]/main/div/div/table[5]/tbody/tr[2]/td[1]').get_attribute('innerHTML')
-print(name)
-
-driver.close()
+print(json.dumps(pokemon_home_jsons, indent=4))
